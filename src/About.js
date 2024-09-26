514dd1a22905346abf1022eee13cfd57beadf08b
Austin Jenkins <austinclarkjenkins@gmail.com>
2024-09-26 08:09:04 -0600

    Add about section

--- src/About.js
+++ src/About.js
@@ -1,5 +1,24 @@
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div className="about">
+			<div className="container">
+				<div className="about-inner">
+					<div>
+						<h2>About Little Lemon</h2>
+						<p>
+							At Little Lemon, we believe that healthy eating should be delicious and accessible for everyone. Located
+							in the heart of Denver, we offer a vibrant menu filled with fresh, locally-sourced ingredients that cater
+							to a variety of dietary needs. From nutrient-packed salads to hearty grain bowls, our dishes are designed
+							to fuel your body and delight your taste buds. Our welcoming atmosphere and friendly staff make it easy to
+							enjoy a wholesome meal whether you’re dining in or on the go. Come visit us and experience the perfect
+							blend of health and flavor!
+						</p>
+					</div>
+					<img src="/about-us.jpg" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
